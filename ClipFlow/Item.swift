44115b3eecc140040eb685bbbbe47047f9e4f63d
Udean777 <uaart123@gmail.com>
2026-06-16 15:15:19 +0700

    Initial Commit

---
+++ ClipFlow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ClipFlow
+//
+//  Created by Sajudin on 16/06/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
